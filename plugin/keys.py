ae1cb14ebc7a6635b3587f4e4e6884279b66b6eb
daGoodenough <dgoodenough@proton.me>
2023-12-27 13:08:56 -0800

    update keys.py

--- plugin/keys.py
+++ plugin/keys.py
@@ -1,8 +1,13 @@
+from typing import Dict
 from hashlib import sha256
+from base64 import b64encode
 from coincurve import PublicKey, PrivateKey
+import math
 
+PrivKeyset = Dict[int, PrivateKey]
+PubKeyset = Dict[int, PublicKey]
 
-def generate_private_keyset(seed, MAX_ORDER, derivation_path):
+def generate_private_keyset(seed, MAX_ORDER: int, derivation_path):
     to_hash = [(seed + derivation_path + str(i)).encode() for i in range(MAX_ORDER)]
     return {
         2 ** idx: PrivateKey(
@@ -11,8 +16,10 @@ def generate_private_keyset(seed, MAX_ORDER, derivation_path):
         for idx, to_hash in enumerate(to_hash)
     }
 
-def get_keyset(priv_keyset):
-    pub_keyset = {}
+def get_keyset(priv_keyset: PrivKeyset):
+    pub_keyset: PubKeyset = {}
     for idx, pk in priv_keyset.items():
         pub_keyset[idx] = pk.public_key
-    return pub_keyset
+    id = derive_keyset_id(pub_keyset)
+    return pub_keyset, id
+
